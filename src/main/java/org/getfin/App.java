866e1171451aace963eadf1575ac5d2f41fb0e7f
cruz2024 <166638436+cruz2024@users.noreply.github.com>
2025-06-08 22:41:47 -0600

    Update App.java

--- src/main/java/org/getfin/App.java
+++ src/main/java/org/getfin/App.java
@@ -6,5 +6,6 @@ package org.getfin;
 public class App {
     public static void main(String[] args) {
         System.out.println("Hola my amor");
+        System.out.println("Creen bien el proyecto");
     }
 }
